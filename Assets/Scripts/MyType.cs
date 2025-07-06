18e9aa6b0b1c8b65be20c214f8748fec9eb373e4
Shinhuichan <94081602shc@gmail.com>
2025-07-06 21:46:25 +0900

    Initial commit

---
+++ Assets/Scripts/MyType.cs
@@ -0,0 +1,14 @@
+
+public class MyType {
+    public enum Unit {Sword, Range, Guard, Wizard, Bullet, Spear, Unicon, Oni, Thrower, King, 
+        Warrior, Hunter, Bomber, Cannon, Pike, Base, Eater, Witch, Monster, WitchUp, 
+        Tower, Farmer, Bully, SuperTower, Linda, Mana, Assasin, Bug, Priest, Light,
+        Herald, Music, Amelia, Mateo, Luna, Henry, Medusa, Sponsor, MedusaUp, WizardT,
+        Emperor, Bodyguard, Reaper, Sniper, Viking, Fence, DefenceTower, Samurai, Sorceror, Giant,
+        Fighter
+    };
+    public enum Sound {Victory, Defeat, Buy, Upgrade, Sword, Range, RangeHit, Guard, Magic, Slash, 
+        Axe, Money, Bomb, Canon, Summon, Hammer, Heal, Error, Destroy, Uprising, 
+        Sheild, Whip, Thunder, Smash, Gun, Reload, Medusa, Bank, Laser, Bite, 
+        Herald, Samurai, Barrior, Giant, Click};
+}
